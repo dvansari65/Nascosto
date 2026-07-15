b5a0cf0e067a1c4b6cae86ed66b173277f387d9a
Danish Ansari <dvansari360@gmail.com>
2026-07-15 23:29:45 +0530

    fix:uploading image to pinata using tanstack

---
+++ frontend/hooks/useImageUpload.ts
@@ -0,0 +1,18 @@
+
+
+import { toast } from "sonner";
+import { uploadFileToIPFS } from "@/pinata/uploadToIpfs";
+import { useMutation } from "@tanstack/react-query";
+
+export function useImageUpload() {
+  return useMutation({
+    mutationKey: ["image"],
+    mutationFn: async (file: File) => {
+      const result = await uploadFileToIPFS(file);
+      return result.ipfsUri;
+    },
+    onError: (e: any) => {
+      toast.error(e?.message || "Image upload failed.");
+    },
+  });
+}
