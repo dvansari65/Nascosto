b81dd3dce1084019952922e7811306de02532c1d
Danish Ansari <dvansari360@gmail.com>
2026-07-18 15:29:50 +0530

    db:configured prisma ORM

---
+++ indexer/lib/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv"
+import { PrismaClient } from "../generated/prisma/client";
+
+dotenv.config()
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL || "",
+});
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient};
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
