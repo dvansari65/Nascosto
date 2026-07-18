b17fa709b521f5ed99f214b98896910e7d8d62ff
Danish Ansari <dvansari360@gmail.com>
2026-07-19 01:36:28 +0530

    feat:add ava's websocket provider to listen on chain events

--- indexer/src/contracts.ts
+++ indexer/src/contracts.ts
@@ -3,17 +3,20 @@ import ShadowCardArtifact from "./abis/ShadowCard.json";
 import MarketplaceArtifact from "./abis/ShadowCardsMarketplace.json";
 import SettlementArtifact from "./abis/ConfidentialSettlement.json";
 import NasToken from "./abis/NasToken.json"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const CONTRACT_ADDRESSES = {
-  SHADOW_CARD: process.env.NEXT_PUBLIC_SHADOW_CARD_ADDRESS || "",
-  MARKETPLACE: process.env.NEXT_PUBLIC_MARKETPLACE_ADDRESS || "",
-  SETTLEMENT: process.env.NEXT_PUBLIC_SETTLEMENT_ADDRESS || "",
-  MINT_TOKEN:process.env.NEXT_PUBLIC_PAYMENT_TOKEN_ADDRESS || ""
+  SHADOW_CARD: process.env.SHADOW_CARD_ADDRESS || "",
+  MARKETPLACE: process.env.MARKETPLACE_ADDRESS || "",
+  SETTLEMENT: process.env.SETTLEMENT_ADDRESS || "",
+  MINT_TOKEN:process.env.PAYMENT_TOKEN_ADDRESS || ""
 };
 
 export function getPublicProvider() {
-  const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc";
-  return new ethers.JsonRpcProvider(rpcUrl);
+  const rpcUrl = "wss://api.avax-test.network/ext/bc/C/ws";
+  return new ethers.WebSocketProvider(rpcUrl);
 }
 
 export function getShadowCardContract(providerOrSigner: ethers.Provider | ethers.Signer) {
