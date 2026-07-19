3276acbec36ef9e67ecdd02f33562245dae08cb6
Danish Ansari <dvansari360@gmail.com>
2026-07-19 19:20:06 +0530

    feat:add socket provider

--- frontend/provider/app-provider.tsx
+++ frontend/provider/app-provider.tsx
@@ -4,14 +4,19 @@ import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { wagmiConfig } from "@/lib/wagmi";
 import { ReactNode, useState } from "react";
-import {WalletProvider} from "./WalletContext"
+import { WalletProvider } from "./WalletContext"
+import { SocketProvider } from "./socket-provider";
 export function AppProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <WalletProvider>{children}</WalletProvider>
+        <WalletProvider>
+          <SocketProvider>
+            {children}
+          </SocketProvider>
+        </WalletProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
