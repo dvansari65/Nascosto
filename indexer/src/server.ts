c67ab9a301fdcfde60621d046023f9c531ac42b2
Danish Ansari <dvansari360@gmail.com>
2026-07-21 11:55:55 +0530

    fix:add dynamic for railway deployment

--- indexer/src/server.ts
+++ indexer/src/server.ts
@@ -6,7 +6,7 @@ import { subscribeRouter, offerRouter, tokenIdsRouter } from "./routes";
 
 export const app = express();
 app.use(cors());
-
+const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +16,7 @@ app.use("/api/tokenIds", tokenIdsRouter);
 
 const server = createServer(app);
 
-server.listen(3001, async () => {
-  console.log("server started!!!", 3001);
+server.listen(PORT, async () => {
+  console.log("server started!!!", PORT);
   await startIndexer(); // start exactly once, at process startup
 });
